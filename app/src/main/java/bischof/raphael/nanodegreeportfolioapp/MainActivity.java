5a5ce594a3c8adcba13eee10b5f0d4a724a96d0f
Biche Biche <raphael.bischof@gmail.com>
2015-06-07 16:50:38 +0200

    Following Instructor's review, MainActivity java code is simplified by using on click in layout instead of setting the listeners programmatically.

--- app/src/main/java/bischof/raphael/nanodegreeportfolioapp/MainActivity.java
+++ app/src/main/java/bischof/raphael/nanodegreeportfolioapp/MainActivity.java
@@ -1,45 +1,33 @@
 package bischof.raphael.nanodegreeportfolioapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_bacon_reader).setOnClickListener(this);
-        findViewById(R.id.btn_build_it_bigger).setOnClickListener(this);
-        findViewById(R.id.btn_capstone_my_own_app).setOnClickListener(this);
-        findViewById(R.id.btn_library_app).setOnClickListener(this);
-        findViewById(R.id.btn_scores_app).setOnClickListener(this);
-        findViewById(R.id.btn_spotify_streamer).setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_bacon_reader:
-                    Toast.makeText(this,getString(R.string.launch_btn_text)+" "+getString(R.string.btn_bacon_reader)+" !",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_build_it_bigger:
-                Toast.makeText(this,getString(R.string.launch_btn_text)+" "+getString(R.string.btn_build_it_bigger)+" !",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_capstone_my_own_app:
-                Toast.makeText(this,getString(R.string.launch_btn_text)+" "+getString(R.string.btn_capstone_my_own_app)+" !",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_library_app:
-                Toast.makeText(this,getString(R.string.launch_btn_text)+" "+getString(R.string.btn_library_app)+" !",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_scores_app:
-                Toast.makeText(this,getString(R.string.launch_btn_text)+" "+getString(R.string.btn_scores_app)+" !",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_spotify_streamer:
-                Toast.makeText(this,getString(R.string.launch_btn_text)+" "+getString(R.string.btn_spotify_streamer)+" !",Toast.LENGTH_SHORT).show();
-                break;
-        }
+
+    /*
+        Show the message depending on the view clicked. OnClick events are defined in the layout file.
+     */
+    public void displayToast(View view) {
+        Button button = (Button) view;
+        String buttonText = (String) button.getText();
+        Context context = getApplicationContext();
+        // open_app would add "Opens the app "
+        CharSequence text = getString(R.string.launch_btn_text)+" "
+                + buttonText+"!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
